a949ddd2e778ebdb396bcd61998989b5237c093c
ICS-Asan <carpediem1015@gmail.com>
2022-08-05 20:45:26 +0900

    solution: 음양더하기 풀이 및 결과

--- Programmers/Level1/[Level1]_5_음양더하기.playground/Contents.swift
+++ Programmers/Level1/[Level1]_5_음양더하기.playground/Contents.swift
@@ -1,1 +1,39 @@
 import UIKit
+
+func solution(_ absolutes:[Int], _ signs:[Bool]) -> Int {
+    var numbers: [Int] = []
+    var result = 0
+    
+    for index in 0..<absolutes.count {
+        if signs[index] == false {
+            numbers.append(absolutes[index] * -1)
+        } else {
+            numbers.append(absolutes[index])
+        }
+    }
+    
+    numbers.forEach {
+        result += $0
+    }
+    return result
+}
+
+print(solution([4,7,12], [true,false,true]))
+print(solution([1,2,3], [false,false,true]))
+
+/*
+<정확성  테스트>
+ 테스트 1 〉    통과 (0.16ms, 16.5MB)
+ 테스트 2 〉    통과 (0.31ms, 16.5MB)
+ 테스트 3 〉    통과 (0.16ms, 16.6MB)
+ 테스트 4 〉    통과 (0.29ms, 16.3MB)
+ 테스트 5 〉    통과 (0.17ms, 16.5MB)
+ 테스트 6 〉    통과 (0.16ms, 16.5MB)
+ 테스트 7 〉    통과 (0.16ms, 16.6MB)
+ 테스트 8 〉    통과 (0.17ms, 16.5MB)
+ 테스트 9 〉    통과 (0.19ms, 16.4MB)
+ 
+<채점 결과>
+ 정확성: 100.0
+ 합계: 100.0 / 100.0
+ */
