d8bc9e95ee868f64def533945e38b083bc253370
Asan <91048487+ICS-Asan@users.noreply.github.com>
2024-09-08 22:38:27 +0900

    solution: Level0_a와 b 출력하기 플이 및 결과

--- Programmers/Level0/[Level0]_02_a와b출력하기.playground/Contents.swift
+++ Programmers/Level0/[Level0]_02_a와b출력하기.playground/Contents.swift
@@ -8,9 +8,30 @@ import Foundation
  - 100,000 ≤ a, b ≤ 100,000
  */
 
+let input = readLine()
+
+if
+    let inputNumbers = input?.components(separatedBy: " ").compactMap({ Int($0) }),
+    inputNumbers.count == 2,
+    inputNumbers[0] >= -100_000,
+    inputNumbers[0] <= 100_000,
+    inputNumbers[1] >= -100_000,
+    inputNumbers[1] <= 100_000
+{
+    print("a = \(inputNumbers[0])")
+    print("b = \(inputNumbers[1])")
+}
+
 /*
  <정확성 테스트>
+ 테스트 1 〉    통과 (4.80ms, 14MB)
+ 테스트 2 〉    통과 (5.73ms, 14.3MB)
+ 테스트 3 〉    통과 (4.29ms, 14MB)
+ 테스트 4 〉    통과 (4.72ms, 14MB)
+ 테스트 5 〉    통과 (5.15ms, 14MB)
  
  <채점 결과>
+ 정확성: 100.0
+ 합계: 100.0 / 100.0
  */
 
