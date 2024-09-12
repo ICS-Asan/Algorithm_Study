f9f5458cfbd0ed572939e6c8241074f2d6d74dd7
Asan <91048487+ICS-Asan@users.noreply.github.com>
2024-09-12 22:13:34 +0900

    solution: Level0_문자열 반복해서 출력하기 풀이 및 결과

--- Programmers/Level0/[Level0]_03_문자열반복해서출력하기.playground/Contents.swift
+++ Programmers/Level0/[Level0]_03_문자열반복해서출력하기.playground/Contents.swift
@@ -10,8 +10,25 @@ import Foundation
  1 ≤ n ≤ 5
  */
 
+let inputs = readLine()?.components(separatedBy: " ")
+
+let string = inputs?[0] ?? ""
+let count = Int(inputs?[1] ?? "") ?? 0
+
+var totalString = ""
+
+for _ in 1...count {
+    totalString += string
+}
+
+print(totalString)
 /*
  <정확성 테스트>
+ 테스트 1 〉    통과 (77.91ms, 13.6MB)
+ 테스트 2 〉    통과 (4.95ms, 14.1MB)
+ 테스트 3 〉    통과 (5.28ms, 14.1MB)
  
  <채점 결과>
+ 정확성: 100.0
+ 합계: 100.0 / 100.0
  */
